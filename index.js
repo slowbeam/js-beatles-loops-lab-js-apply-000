abb68e08ad658da12cfe878f3c25ae7a6db7bd46
Sandy Edwards <peterjamesedwards@gmail.com>
2018-04-18 16:59:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 
   
   function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var facts = ["foo", "bar"];
      var newFacts = [];
      var i = 0;
       while (i < facts.length) {
