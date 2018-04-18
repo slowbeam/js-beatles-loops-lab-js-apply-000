469b130322808fa4e1e49b17f20ad2719b80372e
Sandy Edwards <peterjamesedwards@gmail.com>
2018-04-18 17:02:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     var facts = ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice", "foo", "bar"];
+  "He hated the sound of his own voice"];
      var newFacts = [];
      var i = 0;
       while (i < facts.length) {
