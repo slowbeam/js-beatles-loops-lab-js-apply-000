357e7ad37dba7340b0008db23d2e2bc641e7a30a
Sandy Edwards <peterjamesedwards@gmail.com>
2018-04-18 16:45:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -14,13 +14,13 @@ describe('Beatles Loops', function() {
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
-
+      
       expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
-    });
-  });
+        "foo", 
+        "bar"
+        ])).toEqual(["Foo!!!", "bar!!!"]);
+    }
+
 
   describe('iLoveTheBeatles', function() {
     it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
